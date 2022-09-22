406b30540f41dd03141736bcd13ffca3abf85509
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-09-21 23:24:30 -0500

    Add dependencies: Cors & Concurrently

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import cors from "cors";
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
