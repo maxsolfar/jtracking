d61cc639f7fae5befa48fec407f786957c018a73
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-03 00:27:13 -0500

    Add: Reducer for Login, ToggleSidebar & Logout

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -1,30 +1,42 @@
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER
+} from './actions';
+import { initialState } from './appContext';
 
 export const mainReducer = (state, action) => {
-  if(action.type === DISPLAY_ALERT){
-    return{
+  if (action.type === DISPLAY_ALERT) {
+    return {
       ...state,
       showAlert: true,
-      alertType: "danger",
-      alertText: "Please provide all values!",
-    }
-  };
-  if(action.type === CLEAR_ALERT){
-    return{
+      alertType: 'danger',
+      alertText: 'Please provide all values!',
+    };
+  }
+  if (action.type === CLEAR_ALERT) {
+    return {
       ...state,
       showAlert: false,
-      alertType: "",
-      alertText: "",
-    }
-  };
-  if(action.type === REGISTER_USER_BEGIN){
-    return{
+      alertType: '',
+      alertText: '',
+    };
+  }
+  if (action.type === REGISTER_USER_BEGIN) {
+    return {
       ...state,
-      isLoading: true
-    }
-  };
-  if(action.type === REGISTER_USER_SUCCESS){
-    return{
+      isLoading: true,
+    };
+  }
+  if (action.type === REGISTER_USER_SUCCESS) {
+    return {
       ...state,
       isLoading: false,
       user: action.payload.user,
@@ -32,18 +44,58 @@ export const mainReducer = (state, action) => {
       userLocation: action.payload.location,
       jobLocation: action.payload.location,
       showAlert: true,
-      alertType: "success",
-      alertText: "User Successfully Created!",
-    }
-  };
-  if(action.type === REGISTER_USER_ERROR){
-    return{
+      alertType: 'success',
+      alertText: 'User Successfully Created!',
+    };
+  }
+  if (action.type === REGISTER_USER_ERROR) {
+    return {
       ...state,
       isLoading: false,
       showAlert: true,
-      alertType: "danger",
+      alertType: 'danger',
       alertText: action.payload.msg,
+    };
+  }
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Login Successful! Redirecting...',
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    };
+  }
+  if (action.type === TOGGLE_SIDEBAR) {
+    return { ...state, showSidebar:!state.showSidebar}
+  }
+  if (action.type === LOGOUT_USER) {
+    return {
+      ...initialState,
+      user: null,
+      token: null,
+      userLocation: "",
+      jobLocation: ""
     }
-  };
+  }
   throw new Error(`no such action: ${action.type}`);
 };
