2a864d9f3af79cd49b4617128ce7297fdf38b308
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-03 00:27:47 -0500

    Context for Login, Toggle, Logout

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,28 +1,34 @@
-import React, { useReducer, useContext } from "react";
-import axios from "axios";
-import { mainReducer } from "./reducers";
+import React, { useReducer, useContext } from 'react';
+import axios from 'axios';
+import { mainReducer } from './reducers';
 import {
   CLEAR_ALERT,
   DISPLAY_ALERT,
   REGISTER_USER_BEGIN,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_ERROR,
-} from "./actions";
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER
+} from './actions';
 
 //checking if user exist in localStorage
-const user = localStorage.getItem("user");
-const token = localStorage.getItem("token");
-const userLocation = localStorage.getItem("location");
+const user = localStorage.getItem('user');
+const token = localStorage.getItem('token');
+const userLocation = localStorage.getItem('location');
 
 export const initialState = {
   isLoading: false,
   showAlert: false,
-  alertText: "",
-  alertType: "",
+  alertText: '',
+  alertType: '',
   user: user ? JSON.parse(user) : null,
   token: token || null,
-  userLocation: userLocation || "",
-  jobLocation: userLocation || ""
+  userLocation: userLocation || '',
+  jobLocation: userLocation || '',
+  showSidebar: false,
 };
 
 const AppContext = React.createContext();
@@ -40,35 +46,69 @@ const AppProvider = ({ children }) => {
     }, 2200);
   };
 
-    const registerUser = async (currentUser) => {
+  const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
-      const response = await axios.post("http://localhost:5000/api/v1/auth/register", currentUser);
+      const response = await axios.post(
+        'http://localhost:5000/api/v1/auth/register',
+        currentUser
+      );
       const { user, token, location } = response.data;
       dispatch({
-        type:REGISTER_USER_SUCCESS,
-        payload: {user, token, location}
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
       });
-      addUserToLocalStorage({user, token, location});
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      dispatch({ 
+      dispatch({
         type: REGISTER_USER_ERROR,
-        payload: { msg: error.response.data.msg }
+        payload: { msg: error.response.data.msg },
       });
     }
     clearAlert();
   };
 
+  const loginUser = async (currentUser) => {
+    dispatch({ type: LOGIN_USER_BEGIN });
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/api/v1/auth/login',
+        currentUser
+      );
+      const { user, token, location } = response.data;
+      dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      addUserToLocalStorage({ user, token, location });
+    } catch (error) {
+      dispatch({
+        type: LOGIN_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+
+  const toggleSidebar = () => {
+    dispatch({type: TOGGLE_SIDEBAR})
+  };
+
+  const logoutUser = () => {
+    dispatch({type: LOGOUT_USER});
+    removeUserFromLocalStorage();
+  }
+
   const addUserToLocalStorage = ({ user, token, location }) => {
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("token", token);
-    localStorage.setItem("location", location);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+    localStorage.setItem('location', location);
   };
 
   const removeUserFromLocalStorage = () => {
-    localStorage.removeItem("user");
-    localStorage.setItem("token");
-    localStorage.setItem("location");
+    localStorage.removeItem('user');
+    localStorage.setItem('token');
+    localStorage.setItem('location');
   };
 
   return (
@@ -77,7 +117,10 @@ const AppProvider = ({ children }) => {
         ...state,
         displayAlert,
         clearAlert,
-        registerUser
+        registerUser,
+        loginUser,
+        toggleSidebar,
+        logoutUser
       }}
     >
       {children}
