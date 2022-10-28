1f8c3c7a380eaeb56c5f8a1af3cee215602b7a3d
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-28 16:08:53 -0500

    Add Links to Menu

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,32 +1,51 @@
 import { MdQueryStats } from 'react-icons/md';
 import { ImStatsBars } from 'react-icons/im';
-import { AiOutlineAppstoreAdd} from 'react-icons/ai';
+import { AiOutlineAppstoreAdd, AiOutlineUser } from 'react-icons/ai';
 import { RiProfileLine } from 'react-icons/ri';
+import { BsStackOverflow } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import { FaTasks } from 'react-icons/fa';
+import { BiStats } from 'react-icons/bi';
+
+
+
 
 const links = [
   {
     id: 1,
-    text: 'stats',
+    text: 'overview',
     path: '/dashboard/',
-    icon: <ImStatsBars />,
+    icon: <BsStackOverflow />,
   },
   {
     id: 2,
-    text: 'all jobs',
-    path: '/dashboard/all-jobs',
-    icon: <MdQueryStats />,
+    text: 'stats',
+    path: '/dashboard/stats',
+    icon: <BiStats />,
   },
   {
     id: 3,
+    text: 'all jobs',
+    path: '/dashboard/all-jobs',
+    icon: <FaTasks />,
+  },
+  {
+    id: 4,
     text: 'add job',
     path: '/dashboard/add-job',
     icon: <AiOutlineAppstoreAdd />,
   },
   {
-    id: 4,
+    id: 5,
     text: 'profile',
     path: '/dashboard/profile',
-    icon: <RiProfileLine />,
+    icon: <AiOutlineUser />,
+  },
+  {
+    id: 6,
+    text: 'settings',
+    path: '/dashboard/settings',
+    icon: <FiSettings />,
   },
 ];
 
