673e9a06b2e4c76eb44698b62b404a56ba5f298d
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-27 21:50:06 -0500

    Add ChakraUI, changes on index/app/css

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppProvider } from "./context/appContext";
+import { AppProvider } from './context/appContext';
 import 'normalize.css';
 import './index.css';
 import App from './App';
+import theme from './themes/theme';
+import Fonts from './Fonts';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ColorModeScript } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <Fonts />
+        <App />
+      </ChakraProvider>
     </AppProvider>
   </React.StrictMode>
 );
