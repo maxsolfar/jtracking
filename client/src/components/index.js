da2c623c71bcc070bf05ad1ef9849c5b591f8bcb
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-19 15:05:53 -0500

    Changes All Jobs Components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,6 @@ import Alert from "./Alert.jsx";
 import DesktopSidebar from "./DesktopSidebar";
 import MobileSidebar from "./MobileSidebar";
 import NavBar from "./NavBar";
+import FormRowSelect from "./FormRowSelect";
 
-export { Logo, FormRow, Alert, DesktopSidebar, MobileSidebar, NavBar };
+export { Logo, FormRow, Alert, DesktopSidebar, MobileSidebar, NavBar, FormRowSelect };
--- client/src/pages/Dashboard/AllJobs.js
+++ client/src/pages/Dashboard/AllJobs.js
@@ -1,9 +1,99 @@
 import React from 'react';
+import { FormRow, Alert, FormRowSelect } from '../../components';
+import { useAppContext } from '../../context/appContext';
+
 
 const AllJobs = () => {
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    handleChange
+  } = useAppContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+    console.log('create job');
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    handleChange({ name, value });
+  };
+
   return (
-    <div>AllJobs</div>
-  )
-}
+    <div>
+      <form className="form">
+        <h3>{isEditing ? 'edit job' : 'add job'} </h3>
+        {showAlert && <Alert />}
+
+        {/* position */}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* location */}
+          <FormRow
+            type="text"
+            labelText="location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* job status */}
+
+          <FormRowSelect
+            name="status"
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+
+          {/* job type */}
+          <FormRowSelect
+            labelText="type"
+            name="jobType"
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+
+          <div className="btn-container">
+            <button
+              className="btn btn-block submit-btn"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AllJobs;
