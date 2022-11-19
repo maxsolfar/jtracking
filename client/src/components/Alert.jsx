75b97fe9cfb036d2d6758159677f2d37a3cf9b26
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-19 14:55:36 -0500

    Change Structure in Alerts

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,22 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Alert, AlertIcon } from '@chakra-ui/react';
 import { useAppContext } from '../context/appContext';
 
-const Alert = () => {
+const AlertComponent = () => {
   const { alertType, alertText } = useAppContext();
   return (
-    <Box
-      w={'100%'}
-      bg={alertType === 'danger' ? 'brand.danger' : 'brand.success'}
-      color={"brand.clear"}
-      py={1}
-      px={3}
-      borderRadius={6}
-      textAlign={'center'}
-      fontSize={'xs'}
-    >
-      {alertText}
+    <Box w={'100%'}>
+      {console.log(alertText)}
+      <Alert my={4} mb={4} borderRadius={8} status={alertType === 'danger' ? 'error' : 'success'}>
+        <AlertIcon />
+        {alertText}
+      </Alert>
     </Box>
   );
 };
 
-export default Alert;
+export default AlertComponent;
