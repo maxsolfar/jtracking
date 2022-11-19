0ba2f20975aaf30b8d9d58e98e3640ff9a9a5c53
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-19 15:04:12 -0500

    Add: Job Model

---
+++ models/Job.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const JobSchema = new Schema(
+  {
+    company: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide company name'],
+      maxlength: 80,
+    },
+    position: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide position'],
+      maxlength: 120,
+    },
+    status: {
+      type: String,
+      trim: true,
+      enum: ['Interview', 'Declined', 'Pending'],
+      default: 'Pending',
+    },
+    jobType: {
+      type: String,
+      trim: true,
+      enum: ['Full-time', 'Part-time', 'Remote', 'Internship', 'Hybrid'],
+      default: 'Full-time',
+    },
+    jobLocation: {
+      type: String,
+      trim: true,
+      default: 'My city',
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Job = model('Job', JobSchema);
