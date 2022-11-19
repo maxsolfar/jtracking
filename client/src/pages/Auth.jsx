5b8123bb7ff62f09da5e02dded0f69c593b12164
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-19 14:57:06 -0500

    Changes in Auth Component

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -120,17 +120,6 @@ const Auth = () => {
           w={'700px'}
           ml={'70px'}
         >
-          {/*  <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              fill="#2D3250"
-            ></path>
-          </svg> */}
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -233,18 +222,10 @@ const Auth = () => {
           w={'100%'}
           h={'100%'}
           minH={'700px'}
-          /*           bgImage={{
-            base: 'none',
-            md: bg,
-          }} */
           opacity={0.8}
-          /*           backgroundSize="100%"
-          backgroundPosition="center right"
-          minH={{ base: '100%', md: '700px' }} */
         >
           <Image
             position={'absolute'}
-            /*             borderRadius={"10% 0 0 10%"} */
             h={'100%'}
             objectFit={'cover'}
             src={bg}
