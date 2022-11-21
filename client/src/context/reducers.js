1eadacfeefe63958560e86812aa6f50fd222db22
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-21 00:02:52 -0500

    Add: Get Jobs Reducer

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -17,6 +17,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from './actions';
 import { initialState } from './appContext';
 
@@ -174,6 +176,18 @@ export const mainReducer = (state, action) => {
       alertType: 'danger',
       alertText: action.payload.msg,
     };
+  }
+  if (action.type === GET_JOBS_BEGIN) {
+    return { ...state, isLoading: true, showAlert: false };
+  }
+  if (action.type === GET_JOBS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      jobs: action.payload.jobs,
+      totalJobs: action.payload.totalJobs,
+      numOfPages: action.payload.numOfPages,
+    }
   }
   throw new Error(`no such action: ${action.type}`);
 };
