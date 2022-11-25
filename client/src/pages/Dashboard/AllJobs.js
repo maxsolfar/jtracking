073b139565c0b17fb4059b3d437c043d0ad2a5b7
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-11-25 01:05:59 -0500

    Add: Header's Component

--- client/src/pages/Dashboard/AllJobs.js
+++ client/src/pages/Dashboard/AllJobs.js
@@ -1,12 +1,75 @@
-import { Container } from '@chakra-ui/react';
+import {
+  Stack,
+  Flex,
+  useColorMode,
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
 import React from 'react';
+import { JobsContainer, SearchContainer } from '../../components'
+import { BsSearch } from 'react-icons/bs';
+import { BsPlusLg } from "react-icons/bs";
 
 const AllJobs = () => {
-
+  const { colorMode } = useColorMode();
   return (
-    <Container>
-
-    </Container>
+    <Flex
+      direction={['column', 'column', 'column', 'column']}
+      alignItems={['center', 'center', 'center', 'center']}
+      gap={4}
+    >
+      <Flex
+        w={'100%'}
+        direction={'row'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        borderRadius={16}
+      >
+        <Heading fontSize={'2xl'} color={'brand.secondary'}>
+          My Jobs
+        </Heading>
+        <Stack direction={'row'}>
+          <InputGroup w={'sm'} display={['none', 'none', 'none', 'flex']}>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<BsSearch color="brand.clear" opacity={0.5} />}
+            />
+            <Input
+              focusBorderColor="brand.secondary"
+              type="tel"
+              placeholder="Search here..."
+              bg={colorMode === 'dark' ? 'brand.primary' : 'brand.clear'}
+            />
+          </InputGroup>
+          <Button
+            variant={'solid'}
+            type="submit"
+            colorScheme={'mainBlue'}
+            color={'brand.clear'}
+            fontWeight={'medium'}
+            fontSize={'md'}
+            px={8}
+            leftIcon={<BsPlusLg />}
+          >
+            Add Job
+          </Button>
+        </Stack>
+      </Flex>
+      <Flex
+        w={'100%'}
+        direction={'column'}
+        shadow={'md'}
+        borderRadius={16}
+        p={12}
+        bg={colorMode === 'dark' ? 'brand.primary' : 'brand.clear'}
+      >
+        <SearchContainer />
+        <JobsContainer />
+      </Flex>
+    </Flex>
   );
 };
 
