9faf08a85952fa0ce72a92cecf9117d9b470b829
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-12-03 20:34:05 -0500

    Add: Actions for Edit, Delete Jobs

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -20,3 +20,10 @@ export const CREATE_JOB_ERROR = 'CREATE_JOB_ERROR';
 /* Get Jobs */
 export const GET_JOBS_BEGIN = 'GET_JOBS_BEGIN';
 export const GET_JOBS_SUCCESS = 'GET_JOBS_SUCCESS';
+/* Edit Job */
+export const SET_EDIT_JOB = 'SET_EDIT_JOB';
+export const EDIT_JOB_BEGIN = 'EDIT_JOB_BEGIN';
+export const EDIT_JOB_SUCCESS = 'EDIT_JOB_SUCCESS';
+export const EDIT_JOB_ERROR = 'EDIT_JOB_ERROR';
+/* Delete Job */
+export const DELETE_JOB_BEGIN = 'DELETE_JOB_BEGIN';
