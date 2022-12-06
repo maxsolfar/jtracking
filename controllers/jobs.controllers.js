40206669aceb1d6883bd04409d9f5d1cf92eac5b
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-12-05 19:27:31 -0500

    Controller All Jobs with filters

--- controllers/jobs.controllers.js
+++ controllers/jobs.controllers.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 const jobsControllers = {
   getAllJobs: async (req, res, next) => {
@@ -66,13 +67,47 @@ const jobsControllers = {
     }, {});
 
     const defaultStats = {
-      pending: stats.pending || 0,
-      interview: stats.interview || 0,
-      declined: stats.declined || 0,
+      pending: stats.Pending || 0,
+      interview: stats.Interview || 0,
+      declined: stats.Declined || 0,
     };
 
-    let monthlyApplications = [];
+    console.log(stats.Pending);
 
+    let monthlyApplications = await Job.aggregate([
+      { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+      {
+        $group: {
+          _id: {
+            year: {
+              $year: '$createdAt',
+            },
+            month: {
+              $month: '$createdAt',
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { '_id.year': -1, '_id.month': -1 } },
+      { $limit: 6 },
+    ]);
+
+    monthlyApplications = monthlyApplications
+      .map((item) => {
+        const {
+          _id: { year, month },
+          count,
+        } = item;
+        // accepts 0-11
+        const date = moment()
+          .month(month - 1)
+          .year(year)
+          .format('MMM Y');
+        return { date, count };
+      })
+      .reverse();
+      console.log(defaultStats);
     res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
   },
 };
