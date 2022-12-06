ddb4031d8787a09587a11ac50d58d7343242d093
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-12-05 19:28:16 -0500

    Add Search Component

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../context/appContext';
 
 const SearchContainer = () => {
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
+
+  const handleSearch = (e) => {
+    if (isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
+
   return (
-    <div>SearchContainer</div>
-  )
-}
+    <Flex>
+      <form>
+        <Heading>search form</Heading>
+        {/* search position */}
+        <Box>
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          ></FormRow>
+          {/* search by status */}
+          <FormRowSelect
+            labelText="job status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          ></FormRowSelect>
+          <FormRowSelect
+            labelText='job type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          ></FormRowSelect>
+          {/* sort */}
+
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          ></FormRowSelect>
+          <Button
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </Button>
+        </Box>
+      </form>
+    </Flex>
+  );
+};
 
 export default SearchContainer;
