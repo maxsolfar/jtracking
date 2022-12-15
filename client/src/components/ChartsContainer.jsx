abbe65f1a7cc0cd1d953d854ba664c355abb1402
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-12-15 00:12:04 -0500

    Add: Charts Container Component

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
-import BarChart from "./BarChart";
-import AreaChart from "./AreaChart";
-import { useAppContext } from "../context/appContext";
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import { useAppContext } from '../context/appContext';
+import { Button, Flex, Heading, useColorMode } from '@chakra-ui/react';
 
 const ChartsContainer = () => {
   const [barChart, setBarChart] = useState(true);
-  const { monthlyApllications: data } = useAppContext();
+  const { colorMode } = useColorMode();
+  const { monthlyApplications: data } = useAppContext();
   return (
-    <Flex>
-      <Heading>Monthly Applications</Heading>
-      <Button type='button' onClick={() => setBarChart(!barChart)}>
-        {barChart ? 'AreaChart' : 'BarChart'}
-      </Button>
+    <Flex
+      direction={'column'}
+      px={8}
+      py={4}
+      bg={colorMode === 'dark' ? 'brand.primary' : 'brand.clear'}
+      borderRadius={16}
+      w={"100%"}
+    >
+      <Flex justifyContent={'space-between'} alignItems={'center'}>
+        <Heading fontSize={'2xl'}>Monthly Applications</Heading>
+        <Button
+          type="button"
+          variant={'solid'}
+          bg={barChart ? 'brand.secondary' : '#2ABEB3'}
+          color={'brand.clear'}
+          fontWeight={'normal'}
+          onClick={() => setBarChart(!barChart)}
+        >
+          {barChart ? 'AreaChart' : 'BarChart'}
+        </Button>
+      </Flex>
       {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
     </Flex>
-  )
-}
+  );
+};
 
 export default ChartsContainer;
